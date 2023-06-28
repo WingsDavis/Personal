2b8ebd3c487673adda719feb538cbbe3f656737d
WingsDavis <kofi.10abbandavis@gmail.com>
2023-06-28 17:48:06 +0000

    almost complete

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-contents');
+var previousTab = null;
+
+function opentab(tabname) {
+    var targetElement = document.getElementById(tabname);
+    if (previousTab !== tabname) {
+        for (var i = 0; i < tablinks.length; i++) {
+            tablinks[i].classList.remove('active-link');
+        }
+        for (var i = 0; i < tabcontents.length; i++) {
+            tabcontents[i].classList.remove('active-tab');
+        }
+        targetElement.classList.add('active-tab');
+        previousTab = tabname;
+    }
+}
+
+let index = 0;
+const images = [
+  './images/slide1.png',
+  './images/slide2.jpg',
+  './images/slide3.jpg'
+];
+const slider = document.querySelector('.img-container');
+
+function startSlider() {
+  setInterval(changeBackground, 10000);
+}
+
+function changeBackground() {
+  slider.style.backgroundImage = `url('${images[index]}')`;
+  index = (index + 1) % images.length;
+}
+
+startSlider();
+changeBackground();
